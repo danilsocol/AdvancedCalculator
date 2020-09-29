75b5d2d118f9780ea0a484473898b9927f3ab7c6
danilsocol <danilsocol-2002@mail.ru>
2020-09-29 20:12:40 +0500

    Start

---
+++ labs3/labs3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace labs3
+{
+     class Program
+     {
+
+
+        private static void Main()
+        
+        {
+            Console.WriteLine("Привет,это программа выводит табличку с х и значением функции y=x^2.");
+            int From = Convert.ToInt32(Console.ReadLine());
+            int Before = Convert.ToInt32(Console.ReadLine());
+            if (From> Before)
+            {
+                return;
+            }
+            Console.WriteLine($"X\tY");
+            while (From <= Before ) 
+            {
+                Console.WriteLine($"{From}\t{From * From}");
+                From++;
+                
+            }
+            
+                
+            
+        }
+     }
+}
